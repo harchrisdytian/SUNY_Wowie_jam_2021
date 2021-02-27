8121a62eb2dbd0e40a3d4267a4e2fa8108ae8c0f
harchrisdytian <45524298+harchrisdytian@users.noreply.github.com>
2021-02-26 20:03:13 -0500

    added basic variables
    health and so on. we can do items with packed scenes just because it is easy.

--- Development/Player.gd
+++ Development/Player.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+export(float) var health = 100
+export(float) var speed = 10
+export(PackedScene) var gun
+export(Array, PackedScene) var mods = []
+
 
 func _ready():
 	pass
