3d3d61ca60ac0e777b3f2852ab3fe4dd6317974f
Giovonni <phill240@canton.edu>
2021-02-28 16:08:41 -0500

    connecting upgrade button to player

--- Development/Upgrade_Folder/UpgradeMenu.gd
+++ Development/Upgrade_Folder/UpgradeMenu.gd
@@ -2,6 +2,19 @@ extends CanvasLayer
 
 
 signal OnMiss_U1
+signal OnMiss_U2
+signal OnMiss_U3
+signal OnMiss_U4
+
+signal OnHit_U1
+signal OnHit_U2
+signal OnHit_U3
+signal OnHit_U4
+
+signal OnDeath_U1
+signal OnDeath_U2
+signal OnDeath_U3
+signal OnDeath_U4
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -27,6 +40,8 @@ export var on_death_u4_cost = 400
 
 var adjusted_position = Vector2(-50,-50)
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -36,6 +51,7 @@ func _ready():
 func _process(delta):
 	# Button availability // If the player does not have enough gold 
 	# to buy an upgrade, the upgrades button should be disabled
+	# liekly to be cut in favor of different solution
 	#____ On_Miss_U_Buttons
 	# if player_gold < on_miss_u1_cost:
 		# OnMiss_U1_Button.disabled = true
@@ -225,4 +241,75 @@ func _on_ExitButton_pressed():
 
 func _on_OnMiss_U1_Button_pressed():
 	emit_signal("OnMiss_U1")
+	$MenuBackground/OnMiss_U1_Button.disabled = true
 	pass # Replace with function body.
+
+
+func _on_OnMiss_U2_Button_pressed():
+	emit_signal("OnMiss_U2")
+	$MenuBackground/OnMiss_U2_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnMiss_U3_Button_pressed():
+	emit_signal("OnMiss_U3")
+	$MenuBackground/OnMiss_U3_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnMiss_U4_Button_pressed():
+	emit_signal("OnMiss_U4")
+	$MenuBackground/OnMiss_U4_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnHit_U1_Button_pressed():
+	emit_signal("OnHit_U1")
+	$MenuBackground/OnHit_U1_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnHit_U2_Button_pressed():
+	emit_signal("OnHit_U2")
+	$MenuBackground/OnHit_U2_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnHit_U3_Button_pressed():
+	emit_signal("OnHit_U3")
+	$MenuBackground/OnHit_U3_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnHit_U4_Button_pressed():
+	emit_signal("OnHit_U4")
+	$MenuBackground/OnHit_U4_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnDeath_U1_Button_pressed():
+	emit_signal("OnDeath_U1")
+	$MenuBackground/OnDeath_U1_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnDeath_U2_Button_pressed():
+	emit_signal("OnDeath_U2")
+	$MenuBackground/OnDeath_U2_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnDeath_U3_Button_pressed():
+	emit_signal("OnDeath_U3")
+	$MenuBackground/OnDeath_U3_Button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_OnDeath_U4_Button_pressed():
+	emit_signal("OnDeath_U4")
+	$MenuBackground/OnDeath_U4_Button.disabled = true
+	pass # Replace with function body.
+
+func _on_reset():
+	# set all button.disabled to false
+	pass
