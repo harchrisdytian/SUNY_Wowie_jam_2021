43ceebea6f92315bfb0f61c021dfcdd85693092d
Shakenbake38 <71661085+Shakenbake38@users.noreply.github.com>
2021-02-28 03:02:42 -0500

    removed print statements from enemy bullet and turret

--- Development/Enemies/Turret.gd
+++ Development/Enemies/Turret.gd
@@ -25,7 +25,6 @@ func _physics_process(delta):
 func shoot():
 	if can_shoot:
 		emit_signal("shoot", EnemyBullet, $Position2D.global_position, Vector2(25,0).rotated(rotation))
-		print("shooting")
 		$Shoottimer.start()
 		can_shoot = false
 
