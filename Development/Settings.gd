3899e8b23ca23d4b6f0d4d34769e2a2d94a58f47
KyleFisher22 <polarbearfish7@gmail.com>
2021-02-27 20:26:03 -0500

    Added Volume Scroll Wheel
    I had help adding the Scroll Wheel Volume Script

--- Development/Settings.gd
+++ Development/Settings.gd
@@ -1,4 +1,4 @@
-extends HSlider
+extends Control
 
 
 # Declare member variables here. Examples:
@@ -16,5 +16,5 @@ func _ready():
 #	pass
 
 
-func _on_HSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+func _on_testSound_pressed():
+	$testSound/AudioStreamPlayer.play()
