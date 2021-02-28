fe06638e3dc1db236c68bac70d0c1a6e6d9f81e6
harchrisdytian <45524298+harchrisdytian@users.noreply.github.com>
2021-02-28 17:25:43 -0500

    hmm

--- Development/Player/bullet.gd
+++ Development/Player/bullet.gd
@@ -6,13 +6,14 @@ signal critical_miss
 var crit = false
 var lightnig = preload("res://lightning/lightning .tscn")
 var velocity = Vector2(1,0)
-var lighting_bullet = false
+var lighting_bullet = true
 
 func _process(delta):
 	position += velocity
 
 
 func _on_Bullet_body_entered(body):
+	print("hit anything")
 	if !body.is_in_group("Player"):
 		if body.is_in_group("Enemies"):
 			print("helo")
