9e209cbd72325660dd83d7a269b33c29f8103dba
Shakenbake38 <71661085+Shakenbake38@users.noreply.github.com>
2021-02-28 19:45:03 -0500

    made a level 2 (coins and chest spawns)

--- Development/test_levels/Jordan_test_temp.gd
+++ Development/test_levels/Jordan_test_temp.gd
@@ -8,3 +8,29 @@ func spawnCoins(scene, pos, scale):
 	c.position = pos
 	c.scale = scale
 	add_child(c)
+	$Turret.player = $Player
+
+func shoot(scene,pos,vel,scal):
+	var b = scene.instance()
+	b.position = pos
+	b.velocity = vel
+	b.scale = scal
+	
+	b.connect("hit",$Player,"on_hit")
+	b.connect("hit",self,"on_hit")
+	b.connect("miss",$Player,"on_miss")
+	add_child(b)
+
+func enemy_shoot(scene,pos,vel):
+	var b = scene.instance()
+	b.position = pos
+	b.velocity = vel
+	add_child(b)
+
+
+func on_hit(scene,pos,lightning):
+	print("stonks")
+	if lightning:
+		var l = scene.instance()
+		l.global_position = pos
+		add_child(l)
