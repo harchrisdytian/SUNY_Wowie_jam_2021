beb18ac76221839a21cb5f9d1abf2c1aa870435d
Giovonni <phill240@canton.edu>
2021-02-28 23:57:30 -0500

    Update settingsButton.gd

--- Development/settingsButton.gd
+++ Development/settingsButton.gd
@@ -13,8 +13,17 @@ func _ready():
 
 
 func _on_settingsButton_pressed():
+	$ButtonPressSound.play()
+	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene("res://Settings.tscn")
 
 
 func _on_playButton_pressed():
+<<<<<<< Updated upstream
 	Global.goto_scene("res://Levels/Level1.tscn")
+=======
+	$ButtonPressSound.play()
+	yield(get_tree().create_timer(1), "timeout")
+#	get_tree().change_scene(#Main Scene)
+	pass
+>>>>>>> Stashed changes
